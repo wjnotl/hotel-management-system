96123252543de36622a5898d0ab36589c3e64971
wjnotl <liewwenjing34882@gmail.com>
2026-08-17 00:08:30 +0800

    fix confirmation number null when finding reservation

--- src/repo/ReservationRepo.java
+++ src/repo/ReservationRepo.java
@@ -102,7 +102,9 @@ public class ReservationRepo {
     if (confirmationNumber == null || reservationList == null) return null;
     return reservationList.find(
         r ->
-            r.getConfirmationNumber().equalsIgnoreCase(confirmationNumber)
-                && r.getStatus() == Reservation.Status.CHECKED_IN);
+            r.getConfirmationNumber() == null
+                ? false
+                : r.getConfirmationNumber().equalsIgnoreCase(confirmationNumber)
+                    && r.getStatus() == Reservation.Status.CHECKED_IN);
   }
 }
