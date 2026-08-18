4f028836d2d6601a8b77dad1f7b35c2754ec1643
gabriels06158 <gabrielaugustin1004@gmail.com>
2026-08-18 11:44:24 +0800

    formatting

--- src/control/housekeeping/HouseKeepingController.java
+++ src/control/housekeeping/HouseKeepingController.java
@@ -412,7 +412,9 @@ public class HouseKeepingController {
     HousekeepingSettings settings = settingsRepo.getSettings();
     int max = getMaxRoomsForShift(staff.getShift(), settings);
     int current =
-        staff.getAssignedRoomNumbers() == null ? 0 : staff.getAssignedRoomNumbers().getNumberOfEntries();
+        staff.getAssignedRoomNumbers() == null
+            ? 0
+            : staff.getAssignedRoomNumbers().getNumberOfEntries();
     return current >= max;
   }
 
